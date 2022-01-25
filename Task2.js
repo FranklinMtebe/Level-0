cfccfbc3487bf1d67d8096735058b516dd27312f
FranklinMtebe <96072927+FranklinMtebe@users.noreply.github.com>
2022-01-25 05:15:53 +0200

    Task-02

---
+++ Task2.js
@@ -0,0 +1,17 @@
+// Variable Declaration
+var x;
+var y;
+var z;
+var a;
+var b;
+// Assigning the values of the calculations to variables
+x = 1 + 1 * 2
+y = (1 + 1) * 2
+z = 1 + ( 1 * 2 )
+a = 1 + 1 * 2 / 2
+b = (1 + 1 * 2 ) /  2
+console.log("The new value of x is: " + x)
+console.log("The new value of y is: " + y)
+console.log("The new value of z is: " + z)
+console.log("The new value of a is: " + a)
+console.log("The new value of b is: " + b)
