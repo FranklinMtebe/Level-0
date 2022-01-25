46ed4dbe155128aea47e5171eb3a9be337276bb2
FranklinMtebe <96072927+FranklinMtebe@users.noreply.github.com>
2022-01-25 05:15:02 +0200

    Task-01

---
+++ Task1.js
@@ -0,0 +1,12 @@
+// Declaration and Initialization
+var x = 0;
+var y = 1;
+// Printing to the console
+console.log("The initial value of x is: " + x)
+console.log("The initial value of y is: " + y)
+// Assigning the values of the sum to variables
+x = x + 3
+y = y + x
+// Printing the new values to the console
+console.log("The new value of x is: " + x)
+console.log("The new value of x is: " + y)
