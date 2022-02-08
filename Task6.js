720fd288293ebb1ceeaa2ea0130dafd87946ddac
FranklinMtebe <96072927+FranklinMtebe@users.noreply.github.com>
2022-02-08 10:47:19 +0200

    Task 6 Update

--- Task6.js
+++ Task6.js
@@ -4,7 +4,7 @@ function maximum(number1, number2, number3) {
     } else if (number2 > number1 && number2 > number3) {
       return number2;
     } else if (number3 > number1 && number3 > number1) {
-      //return number3;
+      return number3;
     }
   }
   maximum(1, 2, 3);
